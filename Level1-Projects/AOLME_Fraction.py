33d1298db0c88bd5e07c829c2f7f31d7dc099b0a
ivpcl <65985936+ivpcl@users.noreply.github.com>
2022-08-24 15:48:15 -0600

    Add files via upload

--- Level1-Projects/AOLME_Fraction.py
+++ Level1-Projects/AOLME_Fraction.py
@@ -318,7 +318,7 @@ def vid_show(vid,fps):    #previously aolme_vidshow
         # set the data in the axesimage object
         frame = make_rgb(vid[j])
         im.set_array(frame)
-        #pyplot.draw()
+        pyplot.draw()
         return im,
     # kick off the animation
     if (grid_lines):
@@ -327,8 +327,8 @@ def vid_show(vid,fps):    #previously aolme_vidshow
     else:
         ani = animation.FuncAnimation(fig, update_fig, frames=range(len(vid)),
                                 interval = fps, blit=True, repeat=True)
-    #pyplot.tight_layout()
-    #pyplot.show()
+    pyplot.tight_layout()
+    pyplot.show()
 	
     return ani
 	
